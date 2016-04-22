dffdf05644cca0943495e90f66ffd31d68e671fc
Tamilselvan <tamilselvan@trinaytech.com>
2016-04-22 13:03:05 +0530

    Source code for SF connect

---
+++ salesforce_connect_php/config.php
@@ -0,0 +1,7 @@
+<?php
+	define("CLIENT_ID","3MVG9Y6d_Btp4xp4i_obAsqy7TXk8W38U0wilsXlvUVSFkTCRheWFMPmnFn..YTbvux4NJX5yFg==");
+	define("CLIENT_SECRET","3925819688922917299");
+	define("REDIRECT_URI", "http://localhost:8080/salesforce/oauth_callback.php");
+	define("LOGIN_URI", "https://ap1.salesforce.com");
+?>	
+	
